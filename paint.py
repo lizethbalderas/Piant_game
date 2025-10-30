27034aa1c79c64065de515d90813d1da002a1d6d
Felipe <A01662377@tec.mx>
2025-10-30 11:47:32 -0600

    Funcion para dibujar el rectangulo

--- paint.py
+++ paint.py
@@ -43,16 +43,19 @@ def circle(start, end):
 
 def rectangle(start, end):
     """Draw rectangle from start to end."""
+    up()
     goto(start.x, start.y)
     down()
     begin_fill()
-
-    for count in range(4):
-        forward(end.x - start.x)
-        left(90)
-
+    
+    for _ in range(2):  
+        forward(end.x - start.y)  
+        left(90)  
+        forward(end.y - start.x)  
+        left(90)  
+    
     end_fill()
-
+    
 
 def triangle(start, end):
     """Draw triangle from start to end."""
